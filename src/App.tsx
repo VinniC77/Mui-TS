2807bdd1c54b2d967925aa4c46062f3ca52031b6
viniciusClunc <vinicius@valorizza.com.br>
2023-01-25 13:19:53 -0300

    Themes and Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+import { AppThemeProvider } from "./shared/contexts";
+
 export const App = () => {
   return (
-    <div>
-      App Component
-    </div>
-  )
-}
+    <AppThemeProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AppThemeProvider>
+  );
+};
---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,43 @@
+import { ThemeProvider } from '@mui/material';
+import { DarkTheme, LightTheme } from '../themes';
+import { createContext, useCallback, useState, useMemo, useContext } from "react";
+import { Box } from '@mui/system';
+
+interface IThemeContextData {
+    themeName: 'light' | 'dark';
+    toggleTheme: () => void;
+}
+
+const ThemeContext = createContext({} as IThemeContextData)
+
+export const useAppThemeContext = () => {
+    return useContext(ThemeContext)
+}
+
+interface IAppThemeProviderProps {
+    children: React.ReactNode
+}
+
+export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({ children }) => {
+    const [ themeName, setThemeName ] = useState<'light' | 'dark'>('light')
+
+    const toggleTheme = useCallback(() => {
+        setThemeName(prevThemeName => prevThemeName === 'light' ? 'dark' : 'light')
+    }, [])
+
+    const theme = useMemo(() => {
+        if (themeName === 'light') return LightTheme;
+
+        return DarkTheme;
+    }, [themeName])
+
+    return (
+        <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+            <ThemeProvider theme={theme}>
+                <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+            { children }
+            </Box>
+            </ThemeProvider>
+        </ThemeContext.Provider>
+    )
+}
